fe8d49d93bac7187b58c0fd6d58476b7aa5a2d17
tom-sweers42 <tom.sweers12@gmail.com>
2020-05-31 13:06:54 +0200

    Changed some questions, added fm when diverging from green path. Added fm in level 2

--- Falsification Maze/Assets/Scripts/GameScrips/PlayerCollider.cs
+++ Falsification Maze/Assets/Scripts/GameScrips/PlayerCollider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using System.Linq;
 using UnityEngine.SceneManagement;
 
 using TMPro;
@@ -15,17 +16,31 @@ public class PlayerCollider : MonoBehaviour
     public Material materialRight;
     public Material materialLeft;
     private bool folllowedGreenPath = false;
+    private bool wrongTurn = false;
+    private int wrongCounter = 0;
 
     private String[] fmTexts = new String [] {
-        "Did you notice the green dot pointing in the other direction?",
-        "Did you notice that you followed the blue dot?",
+        "Did you notice the markings on the roof?",
+        "Have you noticed the green dot on the roof?",
         "Did you consider the dots on the roof for your decision?",
-        "Perhaps you should not only use the green path!"
+        "Have you seen the markings?"
     };
 
-    private String [] fmTextsType2 = new String [] {
+    private List<String> fmTextsType1 = new List<String>(){
+        "Did you notice the markings on the roof?",
+        "Have you noticed the green dot on the roof?",
+        "Did you consider the dots on the roof for your decision?",
+        "Have you seen the markings?"
+    };
+
+    private List<String> fmTextsType2 = new List<String>(){
         "Did you see the middle dots turning more red?",
         "Have you looked at the middle dots on the roof?"
+    };
+
+    private List<String> fmTextsType3 = new List<String>(){
+        "Did you look at the floor?",
+        "Did you look at the green tiles?"
     };
     private MazeCell curCheckCell;
     private float time = 0;
@@ -55,7 +70,7 @@ public class PlayerCollider : MonoBehaviour
             if (!folllowedGreenPath && gameManager.checkCell != null && r == gameManager.checkCell.r && c == gameManager.checkCell.c) {
                 if (gameManager.fm.text == "") {
                     System.Random random = new System.Random();
-                    gameManager.fm.text = fmTexts[random.Next(fmTexts.Length)];
+                    gameManager.fm.text = fmTexts[random.Next(fmTextsType1.Count)];
                     folllowedGreenPath = true;
                     Debug.Log("You Followed the greenpath");
                     time = 0;
@@ -85,16 +100,28 @@ public class PlayerCollider : MonoBehaviour
                 if (gameManager.fm.text == "") {
                     Debug.Log("curCheckcel: " + curCheckCell.r + ", " + curCheckCell.c);
                     System.Random random = new System.Random();
-                    gameManager.fm.text = fmTextsType2[random.Next(fmTextsType2.Length)];
+                    gameManager.fm.text = fmTextsType2[random.Next(fmTextsType2.Count)];
                     curCheckCell = null;
                     Debug.Log("You Followed the greenpath");
                     time = 0;
                 }
             }
 
-            if (pathFinishLength != 0 && gameManager.copyMazeCells[r,c].drawRoute(materialPath,0) > pathFinishLength ) {
-                Debug.Log("Wrong Path!!");
-            }
+            // if (pathFinishLength != 0 && gameManager.copyMazeCells[r,c].drawRoute(materialPath,0) > pathFinishLength ) {
+            //     Debug.Log("Wrong Path!!");
+            //     wrongCounter += 1;
+            //     if (gameManager.fm.text == "" && wrongCounter >= 5) {
+            //         System.Random random = new System.Random();
+            //         List<String> combinedList = fmTextsType1.Concat(fmTextsType2).ToList().Concat(fmTextsType3).ToList();
+            //         gameManager.fm.text = combinedList[random.Next(combinedList.Count)];
+            //         folllowedGreenPath = true;
+            //         time = 0;
+            //     }
+            // }
+            // if (pathFinishLength != 0 && gameManager.copyMazeCells[r,c].drawRoute(materialPath,0) < pathFinishLength ) {
+            //     Debug.Log("Correct Path!");
+            //     wrongCounter = 0;
+            // }
             if (folllowedGreenPath) {
                 time += Time.deltaTime;
                 if (time >= 5f) {
@@ -102,9 +129,32 @@ public class PlayerCollider : MonoBehaviour
                 }
             }
             // Debug.Log(pathFinishLength);
-            pathFinishLength = gameManager.copyMazeCells[r,c].drawRoute(materialPath,0);
 
         }
+
+        if (pathFinishLength != 0 && gameManager.copyMazeCells[r,c].drawRoute(materialPath,0) > pathFinishLength ) {
+            Debug.Log("Wrong Path!!");
+            wrongCounter += 1;
+            if (gameManager.fm.text == "" && wrongCounter >= 5) {
+                System.Random random = new System.Random();
+                List<String> combinedList = fmTextsType1.Concat(fmTextsType2).ToList().Concat(fmTextsType3).ToList();
+                gameManager.fm.text = combinedList[random.Next(combinedList.Count)];
+                wrongTurn = true;
+                time = 0;
+            }
+        }
+        if (pathFinishLength != 0 && gameManager.copyMazeCells[r,c].drawRoute(materialPath,0) < pathFinishLength ) {
+            Debug.Log("Correct Path!");
+            wrongCounter = 0;
+        }
+        if (wrongTurn) {
+            time += Time.deltaTime;
+            if (time >= 5f) {
+                gameManager.fm.text = "";
+                wrongTurn = false;
+            }
+        }
+        pathFinishLength = gameManager.copyMazeCells[r,c].drawRoute(materialPath,0);
         gameManager.ClearPath();
         int currPathLength = gameManager.mazeCells[r, c].drawRoute(materialPath, 0);
         gameManager.tilesCounterField.text = currPathLength.ToString();
