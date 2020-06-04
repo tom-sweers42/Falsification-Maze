6c506561b8b778dd37ac8d35899849d080db4fa6
ismailguclu <i.guclu@live.nl>
2020-06-04 20:15:34 +0200

    pause changed + player id

--- Falsification Maze/Assets/Scripts/GameScrips/MainMenu.cs
+++ Falsification Maze/Assets/Scripts/GameScrips/MainMenu.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Text idField;
+
     public void PlayGame ()
     {
-        CrossSceneInformationClass.level += 1;
-        Cursor.visible = false;
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + CrossSceneInformationClass.level);
+        if (idField.text != "")
+        {
+            MazeData mazeData = GameObject.Find("Data").GetComponent<MazeData>();
+            mazeData.playerId = idField.text;
+            CrossSceneInformationClass.level += 1;
+            Cursor.visible = false;
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + CrossSceneInformationClass.level);
+        }
+
     }
 
     public void QuitGame()
